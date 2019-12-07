8da0886bdfbb78b01d0541254f6fd0943c45cb2b
Stephen Saunders <saunders.stephen@gmail.com>
2019-12-06 23:20:36 -0800

    Reset and Normalize css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import reset from 'reset-jss';
+import normalize from 'normalize-jss';
+import jss from 'jss';
+import preset from 'jss-preset-default';
+
+jss.setup(preset());
+jss.createStyleSheet(reset).attach();
+jss.createStyleSheet(normalize).attach();
 
 export default ReactDOM.render(
     <App name="stephen" />,
